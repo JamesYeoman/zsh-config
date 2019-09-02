e3595282df94e39a5639a25b42b3e33035915eb6
James Yeoman <james.yeoman@appsbroker.com>
2019-09-02 13:55:46 +0100

    initial commit

---
+++ profile.zsh
@@ -0,0 +1,22 @@
+source "${SHELL_CONF}/antigen.zsh"
+source "${SHELL_CONF}/aliases.zsh"
+source "${SHELL_CONF}/exports.zsh"
+source "${SHELL_CONF}/functions.zsh"
+
+eval "$(jenv init -)"
+eval "$(pyenv init -)"
+eval "$(nodenv init -)"
+
+# fzf is Fuzzy Find tool https://github.com/junegunn/fzf
+[ -e "${PERSONAL_ETC}/fzf/fzf.zsh" ] && source "${PERSONAL_ETC}/fzf/fzf.zsh"
+
+#This is to allow the themes to be able to display the correct name in the prompt
+export DEFAULT_USER="$(whoami)"
+
+# Sets the window to just the current directory
+export DISABLE_AUTO_TITLE="true"
+case $TERM in
+  xterm*)
+    precmd () {print -Pn "\e]0;%~\a"}
+    ;;
+esac
