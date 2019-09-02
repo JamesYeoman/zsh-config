0fabf14bdbc6d3343122546c378ed3c714843f39
James Yeoman <jamesyeoman10@gmail.com>
2019-09-02 13:55:46 +0100

    initial commit

---
+++ aliases.zsh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+alias commitAll="git add .; git commit"
+alias dir-sizes="du -sh"
+
+#Comment this alias out if you end up using the aliases that uses exa
+alias ls='exa -a --group-directories-first'
+alias ll='ls -l --header'
+alias lgit="ls --git --git-ignore"
+
+alias mvn="mvn --settings ${HOME}/.local/etc/m2/settings.xml"
+
+alias copy_to_clipboard='xclip -selection clipboard'
+
+alias trash='gio trash'
+
+alias stopAllDockerContainers="docker container stop $(docker container ls -aq)"
