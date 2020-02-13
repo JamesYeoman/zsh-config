e1e74d2dd3b14b97d21bb90e827ee90e089a50d2
James Yeoman <jamesyeoman10@gmail.com>
2020-02-13 16:16:13 +0000

    Created an installation helper script

--- prerequisites/install_prerequisites.sh
+++ prerequisites/install_prerequisites.sh
@@ -2,14 +2,17 @@
 
 SCRIPTPATH="$( realpath "$(dirname "$0")" )"
 
-echo "Installing base dependencies"
-bash "${SCRIPTPATH}/deps.sh"
+echo "Installing Docker"
+bash "${SCRIPTPATH}/docker.sh"
+
+echo "Installing ZSH"
+sudo apt install -qqy zsh
 printf "Done\n\n"
 
 echo "Installing Antigen to /usr/local/share/antigen"
 bash "${SCRIPTPATH}/antigen.sh"
 printf "Done\n\n"
 
-echo "Installing a font that has the required glyphs"
+echo "Installing a font that has the required glyphs for the zsh theme"
 bash "${SCRIPTPATH}/zshfont.sh"
 echo "Done"
---
+++ prerequisites/scripts/zsh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo -k apt install -qqy zsh
+chsh -s /bin/zsh
+
