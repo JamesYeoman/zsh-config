49f6579495510fd5331ccc91f89c7b62c7808a78
James Yeoman <jamesyeoman10@gmail.com>
2020-02-17 12:08:23 +0000

    Removed Java 8 from adoptopenjdk installation as jdk 8 is installed by google cloud sdk

--- prerequisites/scripts/adoptopenjdk.sh
+++ prerequisites/scripts/adoptopenjdk.sh
@@ -9,8 +9,7 @@ sudo add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
 echo "Updating the Apt Cache"
 sudo apt update
 
-JDK_VERSIONS=(adoptopenjdk-8-hotspot adoptopenjdk-8-hotspot-jre)
-JDK_VERSIONS+=(adoptopenjdk-11-hotspot adoptopenjdk-11-hotspot-jre)
+JDK_VERSIONS=(adoptopenjdk-11-hotspot adoptopenjdk-11-hotspot-jre)
 JDK_VERSIONS+=(adoptopenjdk-12-hotspot adoptopenjdk-12-hotspot-jre)
 JDK_VERSIONS+=(adoptopenjdk-13-hotspot adoptopenjdk-13-hotspot-jre)
 
