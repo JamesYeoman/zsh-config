0f7408d0affb21b7d48ad4653fde5a1f392cb88d
James Yeoman <jamesyeoman10@gmail.com>
2020-07-02 11:54:10 +0100

    Split prereqs to necessary and optional

--- prerequisites/scripts/jenv.sh
+++ prerequisites/scripts/jenv.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
 JENV_ROOT="${HOME}/.local/share/jenv"
-git clone https://github.com/jenv/jenv.git ${JENV_ROOT}
+git clone "https://github.com/jenv/jenv.git" ${JENV_ROOT}
 
 export PATH="${JENV_ROOT}/bin:${PATH}"
 
 eval "$(jenv init -)"
 
-JDK_VERSIONS=(adoptopenjdk-11-hotspot)
-
-for version in "${JDK_VERSIONS[@]}"; do
-    jenv add "/usr/lib/jvm/${version}-amd64"
+for version in "/usr/lib/jvm"/*/; do
+    jenv add "/usr/lib/jvm/${version}"
 done
 
-jenv enable-plugin maven
-jenv enable-plugin export
+jenv enable-plugin "maven"
+jenv enable-plugin "export"
