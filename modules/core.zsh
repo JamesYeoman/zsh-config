30b5fdf74bde8ae3a688984d22107220ef6e2ac7
James Yeoman <jamesyeoman10@gmail.com>
2020-07-02 14:54:20 +0100

    Configured the ADOTDIR variable
    Now antigen won't be seen in the home directory

--- modules/core.zsh
+++ modules/core.zsh
@@ -1,5 +1,6 @@
 zstyle ':completion:*' completer _complete _ignored
 export CLOUDSDK_HOME="/usr/lib/google-cloud-sdk"
-export HISTFILE="${XDG_CACHE_HOME}/zsh/history"
+export HISTFILE="${XDG_CACHE_HOME:-$HOME/.cache}/zsh/history"
 export EDITOR="vim"
 export ZMODDIR="${ZDOTDIR}/modules"
+export ADOTDIR="${XDG_CACHE_HOME:-$HOME/.cache}/antigen"
