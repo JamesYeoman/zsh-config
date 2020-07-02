44cf05da10bae83a4f89f0bbc43fb50f489096fa
James Yeoman <james.yeoman@appsbroker.com>
2020-07-02 10:16:19 +0100

    Added command guards for aliases
    Commands like exa, xclip and docker aren't installed by default by a
    distro. As a result, the guards are best practice

--- modules/aliases/exa.zsh
+++ modules/aliases/exa.zsh
@@ -1,3 +1,5 @@
-alias ela='exa -al --group-directories-first'
-alias elh='ela --header'
-alias elg="elh --git --git-ignore"
+if testForCommand exa; then
+    alias ela='exa -al --group-directories-first'
+    alias elh='ela --header'
+    alias elg="elh --git --git-ignore"
+fi
