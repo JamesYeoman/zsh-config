481c93647288827159a0978541a4038b256b70cb
James Yeoman <james.yeoman@appsbroker.com>
2020-07-14 15:45:55 +0100

    Sonar + Sonarlint XDG compliance variables

--- modules/xdg.zsh
+++ modules/xdg.zsh
@@ -1,14 +1,19 @@
+conf_home="${XDG_CONFIG_HOME:-$HOME/.config}"
+cache_home="${XDG_CACHE_HOME:-$HOME/.cache}"
+data_home="${XDG_DATA_HOME:-$HOME_ROOT/share}"
+
 # XDG_CONFIG_HOME variables
-export npm_config_userconfig="${XDG_CONFIG_HOME:-$HOME/.config}/npm/npmrc"
+export npm_config_userconfig="${conf_home}/npm/npmrc"
 
 # XDG_CACHE_HOME variables
-export CUDA_CACHE_PATH="${XDG_CACHE_HOME:-$HOME/.cache}/nv"
-export ICEAUTHORITY="${XDG_CACHE_HOME:-$HOME/.cache}/libice/ICEauthority"
-export XAUTHORITY="${XDG_CACHE_HOME:-$HOME/.cache}/xorg-xauth/Xauthority"
-export npm_config_cache="${XDG_CACHE_HOME:-$HOME/.cache}/npm"
+export CUDA_CACHE_PATH="${cache_home}/nv"
+export ICEAUTHORITY="${cache_home}/libice/ICEauthority"
+export XAUTHORITY="${cache_home}/xorg-xauth/Xauthority"
+export npm_config_cache="${cache_home}/npm"
+export SONAR_USER_HOME="${cache_home}/sonar"
+export SONARLINT_USER_HOME="${cache_home}/sonarlint"
 
 # XDG_DATA_HOME variables
-export GRADLE_USER_HOME="${XDG_DATA_HOME:-$HOME_ROOT/share}/gradle"
-export LESSHISTFILE="${XDG_DATA_HOME:-$HOME_ROOT/share}/less/history"
-export MYSQL_HISTFILE="${XDG_DATA_HOME:-$HOME_ROOT/share}/mysql/history"
-
+export GRADLE_USER_HOME="${data_home}/gradle"
+export LESSHISTFILE="${data_home}/less/history"
+export MYSQL_HISTFILE="${data_home}/mysql/history"
