902a4fa5260b6e1f5e987ac57f69838143fa674c
James Yeoman <jamesyeoman10@gmail.com>
2020-11-10 14:10:01 +0000

    Share URL and path segments where possible

--- prerequisites/scripts/gcloud.sh
+++ prerequisites/scripts/gcloud.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-curl -fsSL "https://packages.cloud.google.com/apt/doc/apt-key.gpg" | sudo apt-key add -
-sudo add-apt-repository "deb http://packages.cloud.google.com/apt cloud-sdk main"
+GOOGLE_APT="https://packages.cloud.google.com/apt"
+APT_SOURCE_FILE="/etc/apt/sources.list.d/google-cloud-sdk.list"
+
+echo "deb ${GOOGLE_APT} cloud-sdk main" | sudo tee -a "${APT_SOURCE_FILE}"
+curl -fsSL "${GOOGLE_APT}/doc/apt-key.gpg" | sudo apt-key add -
+
 sudo apt update -qq
 sudo apt install -qqy google-cloud-sdk
--- prerequisites/scripts/yarn.sh
+++ prerequisites/scripts/yarn.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-curl -fsSL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-sudo add-apt-repository "deb https://dl.yarnpkg.com/debian/ stable main"
+YARN_URL="https://dl.yarnpkg.com/debian"
+
+curl -fsSL "${YARN_URL}/pubkey.gpg" | sudo apt-key add -
+sudo add-apt-repository "deb ${YARN_URL}/ stable main"
 sudo apt update -qq && sudo apt install --no-install-recommends yarn
