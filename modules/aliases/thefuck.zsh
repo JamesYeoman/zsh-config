1e4988cbaab2125b6ce6f9c470d9cb4aedd3cb89
James Yeoman <jamesyeoman10@gmail.com>
2021-02-11 12:40:22 +0000

    PIP update-warning changes

--- modules/aliases/thefuck.zsh
+++ modules/aliases/thefuck.zsh
@@ -1,10 +1,13 @@
 # Only enable if user explicitly requests it in the user_defs module
 [[ "${FUCK_YEAH}" != 1 ]] && return
 
+# Prevent PIP from outputting warnings during shell initialisation if out of date
+qWarning="--disable-pip-version-check"
+
 # requires installing the `thefuck` package with pip on python 3
 if testForCommand pyenv && pyenv version-name | grep "3\..*" &>/dev/null; then
-    if ! pip list | grep "thefuck" &> /dev/null; then
-        pip install thefuck
+    if ! pip "$qWarning" list | grep "thefuck" &> /dev/null; then
+        pip "$qWarning" install thefuck
     fi
 
     eval $("thefuck" --alias)
