e3e184f95bd5421294b86b555cdf2d0a36ec36f1
James Yeoman <jamesyeoman10@gmail.com>
2021-02-12 09:44:12 +0000

    Create a Racket installer

---
+++ prerequisites/scripts/racket.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+echo "Adding the Racket APT Repository"
+sudo add-apt-repository --yes ppa:plt/racket
+
+echo "Updating the APT Cache"
+sudo apt update
+
+echo "Installing Racket"
+sudo apt install -qqy racket
+
+echo "Adding a custom Gnome Menu file so that XDG Compliance is session-wide"
+
+COMMENT="DrRacket is an interactive, integrated, graphical programming environment for the Racket programming languages."
+EXEC="env PLTUSERHOME=${XDG_DATA_HOME:-$HOME/.local/share}/racket drracket -singleInstance %F"
+
+cat > "${XDG_CONFIG_HOME:-$HOME/.config}/applications/drracket.desktop" << EOF
+[Desktop Entry]
+Name=DrRacket
+GenericName=IDE for Racket
+Comment=$COMMENT
+Terminal=false
+Type=Application
+Categories=Development;Education;
+Exec=$EXEC
+Icon=racket-logo
+StartupNotify=true
+MimeType=text/x-scheme;
+EOF
