bd21519eeb382a41cbc96c9d302ca33af0c828d1
James Yeoman <jamesyeoman10@gmail.com>
2021-04-06 14:43:44 +0100

    Fix the bootstrap scripts

--- bootstrap/install/update.sh
+++ bootstrap/install/update.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-FOLDER="${dirname $0}"
-verboseLog() {bash -c "${FOLDER}/verboseLog.sh \"$1\""}
+FOLDER="$(realpath $(dirname $0))"
+verboseLog() {
+    bash -c "${FOLDER}/verboseLog.sh \"$1\""
+}
 
 verboseLog "Ensuring everything is up to date"
 sudo apt-get update -qq && sudo apt-get upgrade -qqy
