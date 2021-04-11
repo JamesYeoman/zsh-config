1ba69d0e5c3247b72c3d6f9fa9a3b079126de20c
James Yeoman <jamesyeoman10@gmail.com>
2021-04-11 18:53:35 +0100

    Remove obsolete script

--- prerequisites/copy-to-profile-d.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-for file in "$(realpath $(dirname $0))/profile.d"/*; do
-    local filename="$(basename $file)"
-    [[ ! -f "/etc/profile.d/${filename}" ]] && sudo ln -s "$(realpath $file)" "/etc/profile.d/${filename}"
-done
