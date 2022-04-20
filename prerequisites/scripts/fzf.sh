632c4b562f1710354202e8ffa5c0d70ae3cf1fb2
James Yeoman <james.yeoman@appsbroker.com>
2022-04-20 12:08:16 +0100

    Improve reliability of install scripts

--- prerequisites/scripts/fzf.sh
+++ prerequisites/scripts/fzf.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-source "$(realpath $(dirname $0))/../utils/ensure-variables-exist.sh"
+source "$(realpath $(dirname $0))/../utils/ensure-xdg-folders-exist.sh"
 
-installLoc="${XDG_DATA_HOME}/fzf"
+installLoc="${XDG_DATA_HOME:-${HOME}/.local/share}/fzf"
 
 git clone https://github.com/junegunn/fzf.git "${installLoc}"
 "${installLoc}"/install --all --xdg
