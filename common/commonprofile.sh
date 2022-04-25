71102aa0ad710c2731275f80f054ec3f79507225
James Yeoman <james.yeoman@appsbroker.com>
2022-04-25 11:17:19 +0100

    Additional XDG support

--- common/commonprofile.sh
+++ common/commonprofile.sh
@@ -1,4 +1,15 @@
 #region XDG Home Variables initialisation
+
+# Rather than just falling back, this needs to be done.
+# Some programs are violators of the spec, like Nvidia.
+#
+# The arch wiki states the following
+# "Uses XDG_CACHE_HOME if set, otherwise improperly
+# falls back to ~/.nv instead of ~/.cache."
+#
+# If a multi-million corporation can't understand a
+# specification correctly, then just setting the
+# XDG_CACHE_HOME variable isn't going to cut it...
 if [[ "${XDG_CONFIG_HOME:-unset}" == "unset" ]]; then
     export XDG_CONFIG_HOME="${HOME}/.config"
 fi
@@ -15,10 +26,17 @@ fi
 #region Environment Variables for XDG Compliance
 
 # XDG_CONFIG_HOME variables
-export npm_config_userconfig="${XDG_CONFIG_HOME}/npm/npmrc"
+export NPM_CONFIG_USERCONFIG="${XDG_CONFIG_HOME}/npm/npmrc"
 export DOCKER_CONFIG="${XDG_CONFIG_HOME}/docker"
 export GNUPGHOME="${XDG_CONFIG_HOME}/gnupg"
+export GTK_RC_FILES="${XDG_CONFIG_HOME}/gtk-1.0/gtkrc"
 export GTK2_RC_FILES="${XDG_CONFIG_HOME}/gtk-2.0/gtkrc"
+export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="${XDG_CONFIG_HOME}/java"
+
+VIMINIT_XDG="set nocp | source ${XDG_CONFIG_HOME}/vim/xdgrc.vim"
+export VIMINIT="${VIMINIT_XDG} | source ${XDG_CONFIG_HOME}/vim/vimrc"
+export GVIMINIT="${VIMINIT_XDG} | source ${XDG_CONFIG_HOME}/vim/gvimrc"
+unset VIMINIT_XDG
 
 # XDG_CACHE_HOME variables
 export CUDA_CACHE_PATH="${XDG_CACHE_HOME}/nv"
@@ -33,5 +51,8 @@ export LESSHISTFILE="${XDG_DATA_HOME}/less/history"
 export MYSQL_HISTFILE="${XDG_DATA_HOME}/mysql/history"
 export MACHINE_STORAGE_PATH="${XDG_DATA_HOME}/docker-machine"
 export PLTUSERHOME="${XDG_DATA_HOME}/racket"
-
+export GOPATH="${XDG_DATA_HOME}/go"
+export TERMINFO="${XDG_DATA_HOME}/terminfo"
+export TERMINFO_DIRS="${XDG_DATA_HOME}/terminfo:/usr/share/terminfo"
+export NODE_REPL_HISTORY="${XDG_DATA_HOME}/node_repl_history"
 #endregion
