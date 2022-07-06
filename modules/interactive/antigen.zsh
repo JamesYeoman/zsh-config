376fa4e0dce751d57d32f4c82406d2ceeaf093f9
James Yeoman <james.yeoman@appsbroker.com>
2022-07-06 12:39:58 +0100

    Add support for ZI as alternative to Antigen

--- modules/interactive/antigen.zsh
+++ modules/interactive/antigen.zsh
@@ -1,45 +1,43 @@
 # ANTIGEN_INSTALL should be specified in the interactive init userdef, if required
-if [[ -d "${ANTIGEN_INSTALL:-${XDG_DATA_HOME}/antigen}" ]]; then
-    source "${ANTIGEN_INSTALL:-${XDG_DATA_HOME}/antigen}/antigen.zsh"
-    antigen use "oh-my-zsh"
+source "${ANTIGEN_INSTALL}/antigen.zsh"
+antigen use "oh-my-zsh"
 
-    antigen bundle "git"
-    antigen bundle "command-not-found"
-    antigen bundle "fzf"
+antigen bundle "git"
+antigen bundle "command-not-found"
+antigen bundle "fzf"
 
-    # # Not too useful to the majority of people as the aliases added don't really
-    # # add much beyond situational needs, and the functions added make me feel 
-    # # uneasy (e.g. pyuserpaths). Adding site-packages _blindly_ is just asking for trouble.
-    # antigen bundle "python"
+# # Not too useful to the majority of people as the aliases added don't really
+# # add much beyond situational needs, and the functions added make me feel 
+# # uneasy (e.g. pyuserpaths). Adding site-packages _blindly_ is just asking for trouble.
+# antigen bundle "python"
 
-    if (( $+commands[pip] )); then
-        # Oh My ZSH incorrectly defaults to ~/.pip if $XDG_CACHE_HOME/pip doesn't exist.
-        # See https://github.com/ohmyzsh/ohmyzsh/blob/8f56a8bdf39d7727ab0e220f0164f78c77f9c50e/plugins/pip/pip.plugin.zsh#L15
-        if [[ ! -d "${XDG_CACHE_HOME}/pip" ]]; then
-            mkdir "${XDG_CACHE_HOME}/pip"
-        fi
-
-        # Sets up pip completions.
-        antigen bundle "pip"
+if (( $+commands[pip] )); then
+    # Oh My ZSH incorrectly defaults to ~/.pip if $XDG_CACHE_HOME/pip doesn't exist.
+    # See https://github.com/ohmyzsh/ohmyzsh/blob/8f56a8bdf39d7727ab0e220f0164f78c77f9c50e/plugins/pip/pip.plugin.zsh#L15
+    if [[ ! -d "${XDG_CACHE_HOME}/pip" ]]; then
+        mkdir "${XDG_CACHE_HOME}/pip"
     fi
-    
-    # Sets up rustup and cargo completions.
-    # Handles detecting rustup and cargo automatically
-    # so no need to conditionally load
-    antigen bundle "rust"
 
-    antigen bundle "zsh-users/zsh-syntax-highlighting"
-    antigen bundle "zsh-users/zsh-completions src"
-
-    # A theme from oh-my-zsh
-    #
-    # Visit https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-    # and https://github.com/robbyrussell/oh-my-zsh/wiki/External-themes
-    # to find plenty of alternative themes
-    antigen theme "romkatv/powerlevel10k"
-
-    loadUserDef "interactive/antigen"
-
-    antigen apply
-    antigen selfupdate
+    # Sets up pip completions.
+    antigen bundle "pip"
 fi
+
+# Sets up rustup and cargo completions.
+# Handles detecting rustup and cargo automatically
+# so no need to conditionally load
+antigen bundle "rust"
+
+antigen bundle "zsh-users/zsh-syntax-highlighting"
+antigen bundle "zsh-users/zsh-completions src"
+
+# A theme from oh-my-zsh
+#
+# Visit https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
+# and https://github.com/robbyrussell/oh-my-zsh/wiki/External-themes
+# to find plenty of alternative themes
+antigen theme "romkatv/powerlevel10k"
+
+loadUserDef "interactive/antigen"
+
+antigen apply
+antigen selfupdate
--- modules/interactive/init.zsh
+++ modules/interactive/init.zsh
@@ -1,16 +1,13 @@
-fpath+="${ZDOTDIR}/completions/$USER"
-export -U fpath
-
-if ! (( $+ANTIGEN_INSTALL )); then
-    ANTIGEN_POSSIBLE_LOCATIONS=(
+function antigen_setup() {
+    local possible_locations=(
         "${XDG_DATA_HOME}/antigen" # Install location used by the antigen prerequisites script
         "/usr/share/zsh-antigen"   # sudo apt-get install zsh-antigen
         "${HOME}/.antigen"         # Left over from an old, non-xdg-compliant config, before using this config
     )
 
-    foundLoc="none"
+    local foundLoc="none"
 
-    for loc in $ANTIGEN_POSSIBLE_LOCATIONS; do
+    for loc in $possible_locations; do
         if [[ -d "$loc" ]]; then
             foundLoc="$loc"
             break;
@@ -24,5 +21,38 @@ if ! (( $+ANTIGEN_INSTALL )); then
     else
         export ANTIGEN_INSTALL="$foundLoc"
     fi
-fi
 
+    fpath+="${ZDOTDIR}/completions/$USER"
+    export -U fpath
+}
+
+function zi_setup() {
+    if [[ -z "$ZI" ]]; then
+        echo "Nothing detected in \$ZI and was expecting \$ZI[HOME_DIR] to be set!"
+        echo "Make sure 'typeset -Ag ZI' is defined in your user defs interactive init file!"
+        echo "Make sure that \$ZI[HOME_DIR] is set to the ZI install location (e.g. /opt/zi, which should contain a folder called bin)"
+        return
+    fi
+
+    ZI[BIN_DIR]="${ZI[HOME_DIR]}/bin"
+    ZI[ZCOMPDUMP_PATH]="${HOME}/.cache/zsh/zi/.zcompdump"
+    if [[ ! -d "$(dirname $ZI[ZCOMPDUMP_PATH])" ]]; then
+        mkdir -p "$(dirname $ZI[ZCOMPDUMP_PATH])"
+    fi
+}
+
+case "${ZSH_PLUGIN_MANAGER:-antigen}" in
+    "antigen")
+        antigen_setup
+        ;;
+    "zi")
+        zi_setup
+        ;;
+    *)
+        if [[ -z "$ZSH_PLUGIN_MANAGER_WARN_IGNORE" ]]; then
+            echo "ZSH Baseline Config doesn't have any config for $ZSH_PLUGIN_MANAGER"
+            echo "Please either use one of the supported managers, or handroll your own manager config."
+            echo "If handrolling your own manager config, make sure to set \$ZSH_PLUGIN_MANAGER_WARN_IGNORE"
+        fi
+        ;;
+esac
